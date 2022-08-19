3cff2e78694cffaae178602e21e035f9a4c57d44
Michael Polivka <michael.polivka1@gmail.com>
2022-08-19 10:29:53 +0200

    Snake on screen moving and turning

--- main.py
+++ main.py
@@ -15,17 +15,47 @@ class Snake():
         return self.positions[0]
 
     def turn(self, point):
-        if self.length > 1 and (point[0]* -1, point[1]*-1) == self.direction:
+        if self.length > 1 and (point[0] * -1, point[1]*-1) == self.direction:
             return
         else:
             self.direction = point
 
     def move(self):
-        pass
+        #          [x, y]
+        cur_pos = self.get_head_position()
+        x, y = self.direction
+        new_pos = (((cur_pos[0] + (x*GRID_SIZE)) % SCREEN_WIDTH), (cur_pos[1]+(y*GRID_SIZE)) % SCREEN_HEIGTH)
+        if len(self.positions) > 2 and new_pos in self.positions[2:]:
+            self.reset()
+        else:
+            self.positions.insert(0, new_pos)
+            if len(self.positions) > self.length:
+                self.positions.pop()
+
     def draw(self, surface):
-        pass
+        for pos in self.positions:
+            cube = pygame.Rect((pos[0], pos[1]), (GRID_SIZE, GRID_SIZE))
+            pygame.draw.rect(surface, self.color, cube)
+            pygame.draw.rect(surface, (93, 216, 228), cube, 1)
+
     def handle_keys(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                # nahoru
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
+                    self.turn(UP)
+                #dolu
+                elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                    self.turn(DOWN)
+                #doleva
+                elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                    self.turn(LEFT)
+                #doprava
+                elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                    self.turn(RIGHT)
 
     def reset(self):
         self.length = 1
@@ -37,8 +67,10 @@ class Snake():
 class Food():
     def __init__(self):
         pass
+
     def randomize_position(self):
         pass
+
     def draw(self, surface):
         pass
 
@@ -79,9 +111,15 @@ def main():
     surface = pygame.Surface(screen.get_size())
     surface = surface.convert()
     draw_grid(surface)
+    #vytvoreni nove instance snake a food
+    snake = Snake()
+
     while True:
         clock.tick(10)
+        snake.handle_keys()
         draw_grid(surface)
+        snake.move()
+        snake.draw(surface)
 
         screen.blit(surface, (0, 0))
         pygame.display.update()
